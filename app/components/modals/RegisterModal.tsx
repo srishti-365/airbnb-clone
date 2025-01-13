7bcd7dec9d81e37e44910f9f7316abffba83763a
srishti-365 <137671989+srishti-365@users.noreply.github.com>
2025-01-13 12:42:51 +0530

    Update RegisterModal.tsx

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -36,6 +36,7 @@ const RegisterModal = () => {
         registerModal.onClose();
       })
       .catch((error) => {
+        console.log(error);
         toast.error("Something went wrong!");
       })
       .finally(() => {

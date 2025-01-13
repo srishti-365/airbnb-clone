32f5cad1429cef00e4a996d65d0348a2dcea6174
srishti-365 <137671989+srishti-365@users.noreply.github.com>
2025-01-13 12:42:02 +0530

    Update RegisterModal.tsx

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import Modal from "./Modal";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import useRegisterModal from "@/app/hooks/useRegisterModel";

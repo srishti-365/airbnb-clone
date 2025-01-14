3e662554a1a8bb7e5b30e7832af41844ac32211f
Srishti Sudan <srishtisudan36@gmail.com>
2025-01-14 22:57:35 +0530

    Added prisma, mongodb config with database schema.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import ClientOnly from "./components/ClientOnly";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -23,9 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ClientOnly>
         <ToasterProvider/>
         <RegisterModal />
         <Navbar />
+        </ClientOnly>
       {children}
       </body>
     </html>
